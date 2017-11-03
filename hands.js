55e67071351e3537c5a4a54a83c8a8d8ad83580c
Jmase445 <jmase445@gmail.com>
2017-11-02 19:11:58 -0700

    Add files via upload

---
+++ hands.js
@@ -0,0 +1,100 @@
+function leftPic() {
+	var defaultPic = "images/hdbhDefault.jpg"
+	var orginsPic = "images/hdbhOrgins.jpg"
+	var proofPic = "images/proofOFconcept.jpg"
+	
+	var showing = document.getElementById("currentPic");
+	
+	if (showing.getAttribute('src') == defaultPic) 
+	{
+		showing.setAttribute('src', orginsPic);
+	} 
+	else 
+	{
+		showing.setAttribute('src', defaultPic);
+	}
+	
+}
+
+function rightPic() {
+	var defaultPic = "images/hdbhDefault.jpg"
+	var orginsPic = "images/hdbhOrgins.jpg"
+	var proofPic = "images/proofOFconcept.jpg"
+	
+	
+	var showing = document.getElementById("currentPic");
+	
+	if (showing.getAttribute('src') == defaultPic) 
+	{
+		showing.setAttribute('src', proofPic);
+	} 
+	else 
+	{
+		showing.setAttribute('src', defaultPic);
+	}
+	
+}
+
+
+function checkInfo() {
+	var validity = true;
+	var mailing = document.getElementById("emailAdd").value;
+	var checkEmail = mailing.includes("@");
+	var messageText = "";
+	var infoComplete;
+	
+	try {
+		//if statement to check if checkEmail is false
+		if(checkEmail === false) {
+			throw "Enter an email address with an \"@\" symbol.";
+		} 
+	} 
+	catch(message) {
+		messageText = message;
+	}
+	finally {
+		//checks if email had set to false
+		if(checkEmail === false) {
+			//set validity to false so infoGather doesnt run
+			validity = false;
+		}
+		document.getElementById("crewEmail").innerHTML =  messageText;
+		//see if infoGather should run
+		if(validity === true) {
+		infoGather();
+		}
+	}
+	
+	
+}
+
+
+
+function infoGather() {
+	var mailing = document.getElementById("emailAdd").value;
+	var phone = document.getElementById("phoneNum").value;
+	var age = document.getElementById("getAge").value;
+	var xp = document.getElementById("yearsDrv").value;
+	
+	document.getElementById("crewEmail").innerHTML =  mailing; 
+	document.getElementById("crewNumber").innerHTML = phone;
+	document.getElementById("crewAge").innerHTML = age;
+	document.getElementById("crewYears").innerHTML =  xp; 
+	document.getElementById("driveAge").innerHTML = age - xp;
+	
+}
+/*
+function updateInfo() {
+	infoGather();
+}
+*/
+
+
+		/*  setup variables for the image src */
+			 var rokk = "images/rokk.jpg";
+			 var mase = "images/mase.jpg";
+			 var ault = "images/ault.jpg";
+			 
+document.getElementById("leftButton").addEventListener("click", leftPic, false);
+document.getElementById("rightButton").addEventListener("click", rightPic, false);		 
+document.addEventListener("onclick", checkInfo, false);
