8896cc0df3cc38ea714565e6a8c2225a0658eb8f
Jmase445 <jmase445@gmail.com>
2017-11-22 19:39:42 -0700

    Add files via upload

---
+++ hands7.js
@@ -0,0 +1,17 @@
+$("#Jordan").hover(
+function () {
+	document.getElementById("crewpic").src = "images/rokk.jpg";
+}
+);
+
+$("#jon").hover(
+function () {
+	document.getElementById("crewpic").src = "images/mase.jpg";
+}
+);
+
+$("#james").hover(
+function () {
+	document.getElementById("crewpic").src = "images/ault.jpg";
+}
+);
